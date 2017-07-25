f2cb5484420d7c88665ba0664693840cf040f127
Paul Carduner <paul@carduner.net>
2017-07-25 12:46:42 -0700

    fix failing tests

--- weather-watcher-webapp/app/containers/Database/tests/reducer.test.js
+++ weather-watcher-webapp/app/containers/Database/tests/reducer.test.js
@@ -1,23 +1,9 @@
 import databaseReducer, {initialState} from '../reducer';
-import {
-  receiveNOAAPoint,
-} from '../actions';
+import {receiveNOAAPoint} from '../actions';
 import sampleNOAAPoint from './sampleNOAAPoint.json';
 
 describe('databaseReducer', () => {
   it('returns the initial state', () => {
     expect(databaseReducer(undefined, {})).toEqual(initialState);
-  });
-
-  it('receives noaa points', () => {
-    const noaaPoint = sampleNOAAPoint;
-    const state = databaseReducer(
-      undefined,
-      receiveNOAAPoint({
-        noaaPoint,
-      })
-    );
-    expect(state.get('noaaPoints').valueSeq()).toContain(noaaPoint);
-    expect(state.getIn(['noaaPoints', noaaPoint.id])).toEqual(noaaPoint);
   });
 });
