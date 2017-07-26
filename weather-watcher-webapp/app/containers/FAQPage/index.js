20b84be21d07d265ef4a354ebef62f8111348d66
Meghan Urback <urbackmk@gmail.com>
2017-07-25 18:21:54 -0700

    add padding to faq card

--- weather-watcher-webapp/app/containers/FAQPage/index.js
+++ weather-watcher-webapp/app/containers/FAQPage/index.js
@@ -4,6 +4,10 @@ import styled from 'styled-components';
 import {Card, CardHeader, CardBody} from 'app/components/Card';
 import {OUR_URL, OUR_EMAIL} from 'app/constants';
 
+const FAQWrapper = styled.div`
+  padding-top: ${props => props.theme.padding.standard};
+`;
+
 const List = styled.ul`
   list-style: none;
   padding: 0 ${props => props.theme.padding.standard};
@@ -16,31 +20,33 @@ const ListAnswer = styled.li`padding-bottom: 15px;`;
 export default class FAQPage extends Component {
   render() {
     return (
-      <Card>
-        <CardHeader>
-          <h1>FAQ</h1>
-        </CardHeader>
-        <CardBody>
-          <List>
-            <ListQuestion>How are the scores determined?</ListQuestion>
-            <ListAnswer>Magic!</ListAnswer>
-            <ListQuestion>
-              {`How come a location I'm interested in isn't listed?`}
-            </ListQuestion>
-            <ListAnswer>
-              {`We're still in beta and all of our locations are manually entered
-              by ${OUR_URL} creators. Please email us at ${OUR_EMAIL} with
-              a location that interests you and we'll gladly add it to the
-              list!`}
-            </ListAnswer>
-            <ListQuestion>I found a bug!</ListQuestion>
-            <ListAnswer>
-              {`Thanks for your interest in improving the site. Please email us at
-              ${OUR_EMAIL} and we'll do our best to address it.`}
-            </ListAnswer>
-          </List>
-        </CardBody>
-      </Card>
+      <FAQWrapper>
+        <Card>
+          <CardHeader>
+            <h1>FAQ</h1>
+          </CardHeader>
+          <CardBody>
+            <List>
+              <ListQuestion>How are the scores determined?</ListQuestion>
+              <ListAnswer>Magic!</ListAnswer>
+              <ListQuestion>
+                {`How come a location I'm interested in isn't listed?`}
+              </ListQuestion>
+              <ListAnswer>
+                {`We're still in beta and all of our locations are manually entered
+                by ${OUR_URL} creators. Please email us at ${OUR_EMAIL} with
+                a location that interests you and we'll gladly add it to the
+                list!`}
+              </ListAnswer>
+              <ListQuestion>I found a bug!</ListQuestion>
+              <ListAnswer>
+                {`Thanks for your interest in improving the site. Please email us at
+                ${OUR_EMAIL} and we'll do our best to address it.`}
+              </ListAnswer>
+            </List>
+          </CardBody>
+        </Card>
+      </FAQWrapper>
     );
   }
 }
