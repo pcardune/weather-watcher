deb1a8a66dc4f30b06952d536ec9605161594f6a
Meghan Urback <urbackmk@gmail.com>
2017-07-29 14:14:59 -0700

    start adding more data to comparison point page

--- weather-watcher-webapp/app/containers/ComparisonPointPage/index.js
+++ weather-watcher-webapp/app/containers/ComparisonPointPage/index.js
@@ -1,11 +1,19 @@
-import React, {Component, PropTypes} from 'react';
+import _ from 'lodash';
 import {compose} from 'redux';
 import {connect} from 'react-redux';
+import convert from 'convert-units';
+import React, {Component, PropTypes} from 'react';
+import styled from 'styled-components';
 import {subscribeProps} from 'redux-firebase-mirror';
 
 import {ComparisonPointShape} from 'app/propTypes';
 import {augmentedComparisonPointById} from 'app/containers/Database/subscriptions';
 import {Card, CardHeader, CardBody} from 'app/components/Card';
+
+const ScrollingContainer = styled.div`
+  max-height: 300px;
+  overflow-y: scroll;
+`;
 
 export class ComparisonPointPage extends Component {
   static propTypes = {
@@ -13,14 +21,41 @@ export class ComparisonPointPage extends Component {
   };
 
   render() {
+    const comparisonPoint = this.props.comparisonPoint;
+    const roundedElevationFeet = _.round(
+      convert(comparisonPoint.noaaGridForecast.properties.elevation.value)
+        .from('m')
+        .to('ft')
+    );
     return (
       <Card>
         <CardHeader>
           <h1>
-            {this.props.comparisonPoint.name}
+            {comparisonPoint.name}
           </h1>
         </CardHeader>
-        <CardBody />
+        <CardBody>
+          <div>{`${roundedElevationFeet} ft.`}</div>
+          <ScrollingContainer>
+            {comparisonPoint.noaaDailyForecast.properties.periods.map(period =>
+              <div>
+                <h4>
+                  {period.name}
+                </h4>
+                <div>
+                  <span>
+                    {`${convert(period.temperature)
+                      .from(period.temperatureUnit)
+                      .to('F')} \u2109`}
+                  </span>
+                  <span>
+                    {period.detailedForecast}
+                  </span>
+                </div>
+              </div>
+            )}
+          </ScrollingContainer>
+        </CardBody>
       </Card>
     );
   }
