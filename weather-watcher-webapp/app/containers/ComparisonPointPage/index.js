954120c6990301e0483c71c2c3543edce9be6047
Meghan Urback <urbackmk@gmail.com>
2017-07-30 14:05:38 -0700

    use material css container, loading bar, and connect to score config

--- weather-watcher-webapp/app/containers/ComparisonPointPage/index.js
+++ weather-watcher-webapp/app/containers/ComparisonPointPage/index.js
@@ -9,6 +9,8 @@ import {subscribeProps} from 'redux-firebase-mirror';
 import {ComparisonPointShape} from 'app/propTypes';
 import {augmentedComparisonPointById} from 'app/containers/Database/subscriptions';
 import {Card, CardHeader, CardBody} from 'app/components/Card';
+import LoadingBar from 'app/components/LoadingBar';
+import {selectScoreConfig} from 'app/containers/Database/selectors';
 
 const ForecastPeriodHeader = styled.div`
   margin-bottom: 0px;
@@ -22,53 +24,66 @@ export class ComparisonPointPage extends Component {
 
   render() {
     const comparisonPoint = this.props.comparisonPoint;
+
+    if (!comparisonPoint) {
+      return <LoadingBar />;
+    }
+
     const roundedElevationFeet = _.round(
       convert(comparisonPoint.noaaGridForecast.properties.elevation.value)
         .from('m')
         .to('ft')
     );
     return (
-      <Card>
-        <CardHeader>
-          <h1>
-            {comparisonPoint.name}
-          </h1>
-        </CardHeader>
-        <CardBody>
-          <div className="row">
-            <div className="col s12">
-              {`${roundedElevationFeet} ft.`}
-            </div>
-          </div>
-          <div>
-            {comparisonPoint.noaaDailyForecast.properties.periods.map(period =>
-              <div>
-                <ForecastPeriodHeader className="row">
-                  <div className="col s12">
-                    {period.name}
-                  </div>
-                </ForecastPeriodHeader>
-                <div className="row">
-                  <div className="col s1">
-                    {`${convert(period.temperature)
-                      .from(period.temperatureUnit)
-                      .to('F')} \u2109`}
-                  </div>
-                  <div className="col s11">
-                    {period.detailedForecast}
-                  </div>
-                </div>
+      <div className="container">
+        <Card>
+          <CardHeader>
+            <h1>
+              {comparisonPoint.name}
+            </h1>
+          </CardHeader>
+          <CardBody>
+            <div className="row">
+              <div className="col s12">
+                {`Elevation: ${roundedElevationFeet} ft.`}
               </div>
-            )}
-          </div>
-        </CardBody>
-      </Card>
+            </div>
+            <div>
+              {comparisonPoint.noaaDailyForecast.properties.periods.map(
+                period =>
+                  <div key={period.name}>
+                    <ForecastPeriodHeader className="row">
+                      <div className="col s12">
+                        {period.name}
+                      </div>
+                    </ForecastPeriodHeader>
+                    <div className="row">
+                      <div className="col s1">
+                        {`${convert(period.temperature)
+                          .from(period.temperatureUnit)
+                          .to('F')} \u2109`}
+                      </div>
+                      <div className="col s11">
+                        {period.detailedForecast}
+                      </div>
+                    </div>
+                  </div>
+              )}
+            </div>
+          </CardBody>
+        </Card>
+      </div>
     );
   }
 }
 
 export default compose(
-  connect(null, {}),
+  connect(
+    state => ({
+      scoreConfig: selectScoreConfig(state),
+    }),
+    {}
+  ),
   subscribeProps({
     comparisonPoint: augmentedComparisonPointById,
   })
