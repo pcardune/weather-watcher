e377bcf03c062bc58a8ce2fe23907dc068fd3694
Paul Carduner <paul@carduner.net>
2017-08-20 09:46:09 -0700

    Use matchmediaquery directly instead of react-responsive

--- weather-watcher-webapp/app/components/Responsive.js
+++ weather-watcher-webapp/app/components/Responsive.js
@@ -1,14 +1,48 @@
-import React from 'react';
-import MediaQuery from 'react-responsive';
+import React, {PropTypes} from 'react';
+import matchMedia from 'matchmediaquery';
 
-let VALUE;
-if (process.env.IS_SERVER) {
-  VALUE = {
-    width: 1000,
-  };
+const QUERIES = {
+  desktop: '(min-width: 600px)',
+  phone: '(max-width: 600px)',
+};
+
+function MediaQuery(props) {
+  let matches;
+  if (process.env.IS_SERVER) {
+    matches = props.query === 'desktop';
+  } else {
+    matches = matchMedia(QUERIES[props.query]).matches;
+  }
+
+  if (!matches) {
+    return null;
+  }
+
+  if (typeof props.children === 'function') {
+    return props.children(matches);
+  }
+  const childrenCount = React.Children.count(props.children);
+  const wrapChildren =
+    props.component ||
+    childrenCount > 1 ||
+    typeof props.children === 'string' ||
+    (Array.isArray(props.children) && childrenCount === 1) ||
+    props.children === undefined;
+  if (wrapChildren) {
+    return React.createElement(props.component || 'div', props, props.children);
+  } else if (childrenCount) {
+    return props.children;
+  }
+  return null;
 }
+MediaQuery.propTypes = {
+  component: PropTypes.node,
+  query: PropTypes.oneOf(Object.keys(QUERIES)).isRequired,
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]).isRequired,
+};
+MediaQuery.defaultProps = {
+  component: null,
+};
 
-export const Desktop = ({children}) =>
-  <MediaQuery minWidth={600} children={children} value={VALUE} />;
-export const Phone = ({children}) =>
-  <MediaQuery maxWidth={600} children={children} value={VALUE} />;
+export const Desktop = props => <MediaQuery {...props} query="desktop" />;
+export const Phone = props => <MediaQuery {...props} query="phone" />;
