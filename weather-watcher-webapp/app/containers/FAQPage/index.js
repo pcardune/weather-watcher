2489ebb500425ebfa8c5eec0649436faeaa3f72f
Paul Carduner <paul@carduner.net>
2017-08-23 09:29:54 -0700

    Update FAQ

--- weather-watcher-webapp/app/containers/FAQPage/index.js
+++ weather-watcher-webapp/app/containers/FAQPage/index.js
@@ -5,11 +5,11 @@ import {OUR_URL, OUR_EMAIL} from 'app/constants';
 
 const FAQWrapper = styled.div`
   padding-top: ${props => props.theme.padding.standard};
-`;
 
-const List = styled.ul`
-  list-style: none;
-  padding: 0 ${props => props.theme.padding.standard};
+  ul {
+    list-style: none;
+    padding: 0 ${props => props.theme.padding.standard};
+  }
 `;
 
 const ListQuestion = styled.li`font-weight: bold;`;
@@ -22,10 +22,25 @@ export default class FAQPage extends Component {
       <FAQWrapper>
         <div className="container">
           <h1>FAQ</h1>
-          <List>
+          <ul>
             <ListQuestion>How are the scores determined?</ListQuestion>
-            {/* TODO: Update the score FAQ once we've settled on a scoring algorithm */}
-            <ListAnswer>Magic!</ListAnswer>
+            <ListAnswer>
+              <p>
+                Scores are determined by looking at the temperature, wind,
+                chance of precipitation, and amount of precipitation over the
+                course of the day. Each of these weather factors is assigned a
+                color (red, yellow, or green) based on what range they fall
+                into. For example, a temperature of 65ºF would be considered
+                green, while a temperature of 100ºF would be considered red. If
+                one or more of these weather factors are red for most of the
+                day, then the overall score for that day will appear in red.
+              </p>
+              <p>
+                You can adjust the red, yellow, and green ranges for these
+                weather factors to match your intended activity by clicking on
+                the gear icon above the forecast.
+              </p>
+            </ListAnswer>
             <ListQuestion>
               {"How come a location I'm interested in isn't listed?"}
             </ListQuestion>
@@ -40,7 +55,7 @@ export default class FAQPage extends Component {
               {`Thanks for your interest in improving the site. Please email us at
                 ${OUR_EMAIL} and we'll do our best to address it.`}
             </ListAnswer>
-          </List>
+          </ul>
         </div>
       </FAQWrapper>
     );
