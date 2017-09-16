9bfb443033b117d74be634791002d63cbb3c5f50
Paul Carduner <paul@carduner.net>
2017-09-16 08:12:46 -0700

    move firebase config into constants file

--- weather-watcher-webapp/app/firebaseApp.js
+++ weather-watcher-webapp/app/firebaseApp.js
@@ -1,14 +1,8 @@
 // TODO: only import firebase/app and firebase/database
 import firebase from 'firebase';
+import {firebaseConfig} from './constants';
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyA9dBTF1MZE3jyhjwG37unYMhbQEGurZF4',
-  authDomain: 'weather-watcher-170701.firebaseapp.com',
-  databaseURL: 'https://weather-watcher-170701.firebaseio.com',
-  projectId: 'weather-watcher-170701',
-  storageBucket: 'weather-watcher-170701.appspot.com',
-  messagingSenderId: '936791071551',
-});
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 if (!process.env.IS_SERVER && process.env.NODE_ENV !== 'production') {
   window.firebase = firebaseApp;
